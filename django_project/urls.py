df2e0e4c41411a096a16e5c94b7c39c647951b4e
Eliza Ahmed <elizarafee@gmail.com>
2021-02-23 17:32:19 +0000

    an app created named pets

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vision/', include('vision.urls'))
+    path('vision/', include('vision.urls')),
+    path('pets/', include('pets.urls')),
 ]
