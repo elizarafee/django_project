aca80fc189604ba49151fb92b450eb39f2f59ac8
Eliza Ahmed <elizarafee@gmail.com>
2021-02-23 14:37:07 +0000

    a page returned by using template and sending prop to it

--- vision/views.py
+++ vision/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
+def index(request):
     return HttpResponse("Welcome to the Vision Application!!!")
+
+def greet(request, name):
+    return render(request, 'vision/greet.html', {
+        "name" : name.capitalize()
+    })
