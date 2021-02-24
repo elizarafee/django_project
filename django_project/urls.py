7953d259f171e59d719c56f04d3dd52149013ff7
Eliza Ahmed <elizarafee@gmail.com>
2021-02-24 10:41:28 +0000

    an app created named lists

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('vision/', include('vision.urls')),
     path('pets/', include('pets.urls')),
+    path('lists/', include('lists.urls')),
 ]
