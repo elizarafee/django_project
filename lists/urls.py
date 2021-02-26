6970e497fcdaa0aeb59fde4603d4e97290d714ac
Eliza Ahmed <elizarafee@gmail.com>
2021-02-26 21:15:49 +0000

    used dynamic url for going one page to another page

--- lists/urls.py
+++ lists/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "index"),
+    path("additem", views.addItem, name = "additem"),   #dynamic url uses the  value of the name attribute to determine the path
+    path("list", views.list, name = "list")
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+
+items = ['a', 'b', 'c']
 # Create your views here.
 def index(request):
     return render(request, "lists/index.html")
+
+def addItem(request):
+    return render(request, "lists/additem.html", {
+        "items" : items
+    })
+
+def list(request):
+    return render(request, "lists/list.html", {
+        "items" : items
+    })
