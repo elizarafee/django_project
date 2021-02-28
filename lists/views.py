8cee762e3a315d7f2098a71a4c6d572a67bf187e
Eliza Ahmed <elizarafee@gmail.com>
2021-02-28 20:42:54 +0000

    successfully added items in array by using form

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+class addingItemForm(forms.Form):
+    itemName = forms.CharField(label="Item Name")
+    # priorirty = forms.IntegerField(label="Priority", min_value=3, max_value=6)
 
-items = ['a', 'b', 'c']
+items = []
+
 # Create your views here.
 def index(request):
     return render(request, "lists/index.html")
 
-def addItem(request):
-    return render(request, "lists/addItem.html")
-
 def list(request):
     return render(request, "lists/list.html", {
         "items" : items
+    })
+
+def addItem(request):
+    if request.method == "POST":
+        form = addingItemForm(request.POST)
+        if form.is_valid():
+            itemName = form.cleaned_data["itemName"]
+            items.append(itemName)
+            return HttpResponseRedirect(reverse("lists:list"))
+        else:
+            print("form")
+            return render(request, "lists/addItem.html", {
+            "form": form
+            })
+    
+    return render(request, "lists/addItem.html", {
+        "form": addingItemForm()
     })
